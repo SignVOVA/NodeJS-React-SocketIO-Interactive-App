ff051e564ca3ad7dc083096117800aab822e5a17
Volodymyr Maksymchuk <vovamaksymchuk@gmail.com>
2016-07-22 12:31:37 +0100

    Start the app with npm start. Incopporating sokect io

--- components/APP.js
+++ components/APP.js
@@ -1,9 +1,21 @@
 var React = require('react');
+var io = require('socket.io-client');
 
 var APP = React.createClass({
-	render() {
-		return (<h1>Hello World form React</h1>);
-	}
+
+		componentWillMount() {
+				this.socket = io('http://localhost:3000');
+				this.socket.on('connect', this.connect);
+		},
+
+		connect() {
+				alert("Connected: " +  this.socket.id);
+		},
+
+		render() {
+				return (<h1>Hello World from React</h1>);
+		}
+
 });
 
 module.exports = APP;
