9aed0e9bec0d54facb297f77a142b882a06a5f14
Volodymyr Maksymchuk <vovamaksymchuk@gmail.com>
2016-07-25 12:08:49 +0100

    Issue #7 Creating a display component

--- components/Audience.js
+++ components/Audience.js
@@ -2,15 +2,15 @@ var React = require('react');
 var Display = require('./parts/Display');
 
 var Audience = React.createClass({
-  render() {
-    return (
-      <div>
-        <Display if{this.props.status === 'connected'}>
-          <h1>Join the session</h1>
-        </Display>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Display if={this.props.status === 'connected'}>
+					<h1>Join the session</h1>
+				</Display>
+			</div>
+		);
+	}
 });
 
 module.exports = Audience;
